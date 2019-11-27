2fee20459678fda26ce72d8994a663de013481f2
coltonpo427 <58232793+coltonpo427@users.noreply.github.com>
2019-11-26 20:07:33 -0800

    Delete scroll.js

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-function stopScroll() {
-    $('body').addClass('stop-scrolling'); // adds stop scrolling class to body tag
-    $('body').bind('touchmove', function (e) {
-        e.preventDefault(); // prevents scrolling in body when loaded
-    });
-    $('body').unbind('touchmove'); // allows body scrolling with touch move
-}
